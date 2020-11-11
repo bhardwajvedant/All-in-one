887a914c6671740d8e52e3428b2255f15f2980dd
vedant7172 <55437114+vedant7172@users.noreply.github.com>
2020-11-11 21:00:27 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function abc()
+{
+    var v=prompt("Enter Your Email");
+   
+    if(v)
+    {
+        alert("Varification link has been sent to your Email");
+    }
+    else{
+        prompt("enter a valid Email");
+    }
+}
